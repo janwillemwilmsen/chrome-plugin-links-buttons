c12a18f4f3a93f705d5089a99760e8a83c2d6f92
Jan-Willem Wilmsen <janwillemwilmsen@gmail.com>
2025-05-23 21:29:21 +0200

    remove files again rename files agents.md again

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('openPanel').addEventListener('click', async () => {
-  if (chrome.sidePanel && chrome.sidePanel.open) {
-    await chrome.sidePanel.open({});
-    window.close();
-  }
-});
